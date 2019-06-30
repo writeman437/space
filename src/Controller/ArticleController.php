50ad18ba8517e1645ee31a3f07fbcef279f929cf
jck11 <writeman437@gmail.com>
2019-06-30 23:04:46 +0800

    00210

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Psr\Log\LoggerInterface;
 use Michelf\MarkdownInterface;
 use Symfony\Component\Cache\Adapter\AdapterInterface;
+use App\Service\MarkdownHelper;
 
 class ArticleController extends AbstractController
 {
@@ -22,7 +23,7 @@ class ArticleController extends AbstractController
     /**
      * @Route("/news/{slug}", name="article_show")
      */
-    public function show($slug, MarkdownInterface $markdown, AdapterInterface $cache)
+    public function show($slug, MarkdownHelper $markdownHelper)
     {
         $comments = [
             'I ate a normal rock once. It did NOT taste like bacon!',
@@ -47,15 +48,9 @@ class ArticleController extends AbstractController
         fugiat.
         EOF;
 
-        $item = $cache->getItem('markdown_'.md5($articleContent));
-        if (!$item->isHit()) {
-            $item->set($markdown->transform($articleContent));
-            $cache->save($item);
-        }
-        $articleContent = $item->get();
-
-        $articleContent= $markdown->transform($articleContent);
-
+        $articleContent = $markdownHelper->parse(
+            $articleContent,
+        );
 
         return $this->render('article/show.html.twig', [
             'title' => ucwords(str_replace('-', ' ', $slug)),
---
+++ src/Service/MarkdownHelper.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Service;
+
+use Michelf\MarkdownInterface;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
+use Psr\Log\LoggerInterface;
+
+class MarkdownHelper
+{
+    private $cache;
+    private $markdown;
+    private $logger;
+
+    public function __construct(AdapterInterface $cache, MarkdownInterface $markdown, LoggerInterface $markdownLogger)
+    {
+        $this->cache = $cache;
+        $this->markdown = $markdown;
+        $this->logger = $markdownLogger;
+    }
+
+    public function parse(string $source): string
+    {
+        if (stripos($source, 'bacon') !== false) {
+          $this->logger->info('They are talking about bacon again!');
+        }
+
+        $item = $this->cache->getItem('markdown_'.md5($source));
+        if (!$item->isHit()) {
+            $item->set($this->markdown->transform($source));
+            $this->cache->save($item);
+        }
+
+        return $item->get();
+    }
+}
